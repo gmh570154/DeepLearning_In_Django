5f1885d3fa848a2c322d24589a5a349789c90be6
gmh570154 <39473892+gmh570154@users.noreply.github.com>
2019-05-19 02:33:38 +0800

    add print log
    add print log and run time

--- word2vec/train_text3.py
+++ word2vec/train_text3.py
@@ -7,14 +7,15 @@ LabeledSentence = gensim.models.doc2vec.LabeledSentence
 path = os.getcwd()
 size = 100
 import random
-
-with open(path[:-9] + '\\aclImdb\\train\\pos_all.txt', 'r') as infile:
+import datetime
+start = datetime.datetime.now()
+with open(path[:-9] + '\\aclImdb\\train\\pos_all.txt', 'r', encoding="utf-8") as infile:
     pos_tweets = infile.readlines()
 
-with open(path[:-9] + '\\aclImdb\\train\\neg_all.txt', 'r') as infile:
+with open(path[:-9] + '\\aclImdb\\train\\neg_all.txt', 'r', encoding="utf-8") as infile:
     neg_tweets = infile.readlines()
 
-with open(path[:-9] + '\\aclImdb\\train\\unsup_all.txt', 'r') as infile:
+with open(path[:-9] + '\\aclImdb\\train\\unsup_all.txt', 'r', encoding="utf-8") as infile:
     unsup_reviews = infile.readlines()
 
 # 1 代表积极情绪，0 代表消极情绪
@@ -33,11 +34,11 @@ def cleanText(corpus):
     corpus = [z.split() for z in corpus]
     return corpus
 
-
+print("start clean Text")
 x_train = cleanText(x_train)
 x_test = cleanText(x_test)
 unsup_reviews = cleanText(unsup_reviews)
-
+print("end clean text")
 
 # Gensim 的 Doc2Vec 工具要求每个文档/段落包含一个与之关联的标签。我们利用 LabeledSentence 进行处理。格式形如 “TRAIN_i” 或者 “TEST_i”，其中 “i” 是假的评论索引。
 def labelizeReviews(reviews, label_type):
@@ -47,7 +48,7 @@ def labelizeReviews(reviews, label_type):
         labelized.append(LabeledSentence(v, [label]))
     return labelized
 
-
+print("start labelize")
 x_train = labelizeReviews(x_train, 'TRAIN')
 x_test = labelizeReviews(x_test, 'TEST')
 unsup_reviews = labelizeReviews(unsup_reviews, 'UNSUP')
@@ -61,10 +62,11 @@ a = []
 a.extend(x_train)
 a.extend(x_test)
 a.extend(unsup_reviews)
+print("before build")
 # 对所有评论创建词汇表
 model_dm.build_vocab(a)
 model_dbow.build_vocab(a)
-
+print("end build vocab")
 # 多次传入数据集，通过每次滑动（shuffling）来提高准确率。
 all_train_reviews = np.concatenate((x_train, unsup_reviews))
 b =[]
@@ -76,46 +78,48 @@ def sentences_perm(sentences):
     random.shuffle(shuffled)
     return (shuffled)
 
-
-for epoch in range(10):
+print("before train")
+for epoch in range(1):
     model_dm.train(sentences_perm(b), total_examples=model_dm.corpus_count,epochs=1)
     model_dbow.train(sentences_perm(b), total_examples=model_dbow.corpus_count,epochs=1)
-
+print("end all train")
 
 # 从我们的模型中获得训练过的向量
 def getVecs(model, corpus, size):
     vecs = [np.array(model[z.tags[0]]).reshape((1, size)) for z in corpus]
     return np.concatenate(vecs)
 
-
+print("before get vecs")
 train_vecs_dm = getVecs(model_dm, x_train, size)
 train_vecs_dbow = getVecs(model_dbow, x_train, size)
 train_vecs = np.hstack((train_vecs_dm, train_vecs_dbow))
-
+print("end get vecs")
 # 训练测试数据集
 c = []
 c.extend(x_test)
-
-for epoch in range(10):
+print("before last train")
+for epoch in range(1):
     model_dm.train(sentences_perm(c), total_examples=model_dm.corpus_count,epochs=1)
     model_dbow.train(sentences_perm(c), total_examples=model_dbow.corpus_count,epochs=1)
-
+print("end last train")
 # 创建测试数据集向量
 test_vecs_dm = getVecs(model_dm, x_test, size)
 test_vecs_dbow = getVecs(model_dbow, x_test, size)
 test_vecs = np.hstack((test_vecs_dm, test_vecs_dbow))
-
+print("end get last vecs")
 from sklearn.linear_model import SGDClassifier
-
+print("start show")
 lr = SGDClassifier(loss='log', penalty='l1')
 lr.fit(train_vecs, y_train)
 
-print 'Test Accuracy: %.2f' % lr.score(test_vecs, y_test)
-
+print('Test Accuracy: %.2f' % lr.score(test_vecs, y_test))
+end = datetime.datetime.now()
+print("运行时长：")
+print(end - start)
 from sklearn.metrics import roc_curve, auc
 
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 pred_probas = lr.predict_proba(test_vecs)[:, 1]
 
