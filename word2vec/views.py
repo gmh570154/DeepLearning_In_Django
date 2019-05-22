dfe1762837bc812730e1c09afb0ff4d5a26ae3b1
gmh <39473892+gmh570154@users.noreply.github.com>
2019-05-22 18:28:01 +0800

    修改请求方式
    修改请求方式及返回格式

--- word2vec/views.py
+++ word2vec/views.py
@@ -8,4 +8,4 @@ from bson.json_util import dumps
 def get_ganji_all(request):  # 获取赶集网爬虫的所有数据
     db = mongodbHelper.mongoDB()
     data = db.find_all("ganjicol")
-    return HttpResponse(dumps(data,ensure_ascii=False),content_type="application/json,charset=utf-8")
+    return HttpResponse(dumps({"result":data,"errorCode":0}, ensure_ascii=False), content_type="application/json,charset=utf-8")
